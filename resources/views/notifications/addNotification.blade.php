8af28676724d239522a801f03047afca8807ec54
Timothy Kibalama <timkibalama@gmail.com>
2023-06-23 00:01:26 +0300

    added build folder

--- resources/views/notifications/addNotification.blade.php
+++ resources/views/notifications/addNotification.blade.php
@@ -2,38 +2,39 @@
 
 @section('content')
 
-@if(Session::has('notifications.addNotification'))
-   <span>{{Session::get('notifications.addNotification')}}</span>
-@endif
+    @if(Session::has('notifications.addNotification'))
+        <span>{{Session::get('notifications.addNotification')}}</span>
+    @endif
 
-<div class="card card-info">
-              <div class="card-header">
-                <h3 class="card-title">Add Notification</h3>
-              </div>
-              <!-- /.card-header -->
-              <!-- form start -->
-              <form action="{{ route('addNotification') }}" method="post">
-               @csrf
-                <div class="card-body">
-                  <div class="form-group">
+    <div class="card card-info">
+        <div class="card-header">
+            <h3 class="card-title">Add Notification</h3>
+        </div>
+        <!-- /.card-header -->
+        <!-- form start -->
+        <form action="{{ route('addNotification') }}" method="post">
+            @csrf
+            <div class="card-body">
+                <div class="form-group">
                     <label for="title">Title</label>
                     <input type="text" class="form-control" id="title" placeholder="Enter title" name="title">
-                  </div>
+                </div>
 
-                  <div class="form-group">
+                <div class="form-group">
                     <label for="message">Message</label>
                     <textarea class="form-control" name="message" id="message" cols="20" rows="5"></textarea>
-                  </div>
+                </div>
 
-                  <div class="form-group">
+                <div class="form-group">
                     <label for="longitude">Date</label>
                     <input type="date" class="form-control" id="date" placeholder="date" name="date">
-                  </div>
+                </div>
                 <!-- /.card-body -->
                 <div class="card-footer">
-                  <button type="submit" class="btn btn-info">Submit</button>
+                    <button type="submit" class="btn btn-info">Submit</button>
                 </div>
-              </form>
             </div>
-            <!-- /.card -->
+        </form>
+    </div>
+    <!-- /.card -->
 @endsection
