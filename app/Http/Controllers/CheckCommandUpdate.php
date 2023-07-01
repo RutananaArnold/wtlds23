37901217e58e244dde58986970d002abae42fcbd
RutananaArnold <69462547+RutananaArnold@users.noreply.github.com>
2023-07-01 19:27:16 +0300

    Update CheckCommandUpdate.php
    returned json response in checkCommand

--- app/Http/Controllers/CheckCommandUpdate.php
+++ app/Http/Controllers/CheckCommandUpdate.php
@@ -19,7 +19,8 @@ class CheckCommandUpdate extends Controller
         $device = Devices::where('id', $deviceId)->first();
         if ($device) {
              // Return the response to the Arduino
-            return response("open_valve");
+            // return response("open_valve");
+               return response()->json(['message' => 'open_valve'], 201);
                 
         }    
             
